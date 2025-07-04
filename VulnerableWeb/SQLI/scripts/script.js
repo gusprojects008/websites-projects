3ce719faf78123d401231d62ee871f7b0ca2fb0d
gusprojects008 <gus0100@tutamail.com>
2025-07-04 17:14:29 -0300

    Developing http header and URLSearchParams to endpoint

--- VulnerableWeb/SQLI/scripts/script.js
+++ VulnerableWeb/SQLI/scripts/script.js
@@ -1,5 +1,8 @@
+// frontend validatioan!
 const username = document.getElementById("username");
 const userComment = document.getElementById("user-comment");
+//let username = document.getElementById("username");
+//let userComment = document.getElementById("user-comment");
 const sendData = document.getElementById("send-data");
 const userComments = document.getElementById("users-comments");
 
@@ -8,11 +11,21 @@ sendData.addEventListener("click", async (e) => {
   const usernameValue = username.value.trim();
   const userCommentValue = userComment.value.trim();
 
-  if (!usernameValue) {
+  if (!usernameValue || usernameValue.length > 32) {
      alert("Insira um username de até 32 caracteres! (Enter a username of up 32 characters!)");
+     return; // Stops function execution after executing "alert()"
+     };
+  if (!userCommentValue || userCommentValue.length > 32) {
+     alert("Por favor, insira um comentário de até 32 caracteres! a(Please enter a comment of up to 32 charecters!)");
      return;
-     }
+     };
+
+  const userDataValues = new URLSearchParams();
+  userDataValues.append("username", usernameValue);
+  userDataValues.append("comment", userCommentValue);
   
+  try {
+    const res = await fetch("/VulnerableWeb/SQLI/scripts/endpointToAPI")
+  } catch (error) {};
 });
 
-  const userPost = {};
